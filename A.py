80b16dd8e34c1dcd068ae8ca0fba37d34dda333c
abushaa <alishaabu@mail.ru>
2015-12-08 10:06:35 +0300

    improved A

--- A.py
+++ A.py
@@ -20,8 +20,6 @@
 f = open('input.txt')
 N = int(f.readline())
 A = f.readline().split()
-#N=int(input())
-#A = list(map(int, input().split()))
 for i in range(len(A)):
     A[i] = int(A[i])
 
@@ -29,6 +27,7 @@ for i in range(len(A) - 1):
     for j in range(i + 1, len(A)):
         if A[i] == A[j]:
             x = A[i]
+
 f = open('output.txt', 'w')
 print(x, file = f)
 f.close()
