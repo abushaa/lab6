f9d4c08406bab06b2d9c746a7b88a38c5651c07c
abushaa <alishaabu@mail.ru>
2015-12-08 09:50:03 +0300

    ex A

--- A.py
+++ A.py
@@ -16,3 +16,14 @@
 | 8 3 5 4 5 1 |       |
 +-------------+-------+
 '''
+
+N=int(input())
+A = list(map(int, input().split()))
+for i in range(len(A)):
+    A[i] = int(A[i])
+
+for i in range(len(A) - 1):
+    for j in range(i + 1, len(A)):
+        if A[i] == A[j]:
+            x = A[i]
+print(x)
