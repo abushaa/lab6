1c25f6d2da3315eddc1c822e6d4e566ba47017d5
abushaa <alishaabu@mail.ru>
2015-12-08 10:13:47 +0300

    changed A back to previous state, less fails

--- A.py
+++ A.py
@@ -23,11 +23,12 @@ A = f.readline().split()
 for i in range(len(A)):
     A[i] = int(A[i])
 
-for i in range(len(A)):
-    for j in range(len(A)):
+for i in range(len(A) - 1):
+    for j in range(i +1, len(A)):
         if A[i] == A[j]:
             x = A[i]
 
 f = open('output.txt', 'w')
 print(x, file = f)
 f.close()
+
